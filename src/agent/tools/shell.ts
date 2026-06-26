22a05c4107c8dcd1abca6630a1d4b83e678862d4
lucsa <seyadeodin@gmail.com>
2026-06-26 01:57:23 -0300

    add sheel capabilities

--- src/agent/tools/shell.ts
+++ src/agent/tools/shell.ts
@@ -1,3 +1,29 @@
-import { openai } from "@ai-sdk/openai";
+import { tool } from "ai";
+import shell from "shelljs";
+import z from "zod";
 
-export const runCommand = openai.tools.shell({})
+export const runCommand = tool({
+	description: `Execute a shell command and return an output. Use this for system operations, running scripts, or interacting with the operating system.`,
+	inputSchema: z.object({
+		command: z.string().describe("The shell command to execute"),
+	}),
+	execute: ({ command }: { command: string }) => {
+		const result = shell.exec(command, { silent: true });
+
+		let output = "";
+
+		if (result.stdout) {
+			output += result.stdout;
+		}
+
+		if (result.stderr) {
+			output += result.stderr;
+		}
+
+		if (result.code !== 0) {
+			return `Command failed (exite code ${result.code})\nOUTPUT: ${output}`;
+		}
+
+		return output || `Command ${command} completed successfully (no output)`;
+	},
+});
